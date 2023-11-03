6f07c9bb5712e3ab22e63b9f8dadf07d5d301ba3
rssxda <rssxda.dev@icloud.com>
2023-11-03 17:45:36 -0300

    sm6225-common: remove GNSS packages from device makefiles

--- bengal.mk
+++ bengal.mk
@@ -206,15 +206,7 @@ PRODUCT_PACKAGES += \
 # GPS
 PRODUCT_PACKAGES += \
     android.hardware.gnss@2.1-impl-qti \
-    android.hardware.gnss@2.1-service-qti \
-    gnss@2.0-base.policy \
-    gnss@2.0-xtra-daemon.policy \
-    gnss@2.0-xtwifi-client.policy \
-    gnss@2.0-xtwifi-inet-agent.policy \
-    libbatching \
-    libgeofencing \
-    libgnss \
-    libloc_socket
+    android.hardware.gnss@2.1-service-qti
 
 # GPS
 LOC_HIDL_VERSION := 4.0
